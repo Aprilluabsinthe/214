345ad67ce0d0f0106cc60612a2d6103de86af2a5
Charlie Garrod <charlie@cs.cmu.edu>
2021-02-12 13:56:07 -0500

    Staff add homework 2 [ci skip]

---
+++ homework/2/src/main/java/edu/cmu/cs/cs214/hw2/Interpret.java
@@ -0,0 +1,13 @@
+package edu.cmu.cs.cs214.hw2;
+
+/**
+ * Main program that runs the assembly language interpreter
+ */
+public class Interpret {
+    /**
+     * The main method to run the assembly language interpreter
+     * @param args    program arguments
+     */
+    public static void main(String[] args) {
+    }
+}
