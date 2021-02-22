2b180f208ec7b1759cf130c4d5b85830adb5f33d
Charlie Garrod <charlie@cs.cmu.edu>
2021-02-22 17:10:53 -0500

    Staff add homework 3 [ci skip]

---
+++ homework/3/src/main/java/edu/cmu/cs/cs214/hw3/cryptarithm/expression/CryptarithmExpressionContext.java
@@ -0,0 +1,23 @@
+package edu.cmu.cs.cs214.hw3.cryptarithm.expression;
+
+/**
+ * The context for evaluating a cryptarithm expression. An instance of this class represents a mapping
+ * from the letters that occur in a cryptarithm to digits.
+ */
+public class CryptarithmExpressionContext {
+    // Replace this comment with the field(s) in your Context object.
+    // This object is used repeatedly in testing the validity of a proposed solution,
+    // so your choice of representation can impact the performance of your solver.
+
+    /**
+     * Returns a context that associates the given letters with the given digits. What
+     * parameter(s) you pass to this constructor, and how they are incorporated into the
+     * context object are up to you. 
+     */
+    public CryptarithmExpressionContext( /* replace this comment with your parameter list */ ) {
+        // Replace the statement below with your own implementation
+        throw new UnsupportedOperationException();
+    }
+
+    // Add any method(s) you need in order to implement CryptarithmWordExpression.eval()
+}
---
+++ homework/3/src/main/java/edu/cmu/cs/cs214/hw3/cryptarithm/expression/CryptarithmWordExpression.java
@@ -0,0 +1,31 @@
+package edu.cmu.cs.cs214.hw3.cryptarithm.expression;
+
+/**
+ * An expression representing a cryptarithm word.
+ */
+public class CryptarithmWordExpression implements CryptarithmExpression {
+    // Declare whatever field(s) you feel are appropriate.
+    // Several of these expressions will be evaluated each time you test a proposed
+    // solution, so your representation can affect the performnce of your solver.
+     
+    /** Constructs a cryptarithm expression corresponding to the given word.
+     * @param word  The word which this cryptarithm expression should represent.
+     */
+    public CryptarithmWordExpression(String word) {
+        // Replace the statement below with your own implementation
+        throw new UnsupportedOperationException();
+    }
+
+    /** Returns the value of this word expression for the given context (i.e., letter-digit mapping). */
+    @Override
+    public long eval(CryptarithmExpressionContext ctx) {
+        // Replace the statement below with your own implementation
+        throw new UnsupportedOperationException();
+    }
+
+    /** Returns the word represented by this object (e.g., "SEND") */
+    @Override public String toString() {
+        // Replace the statement below with your own implementation
+        throw new UnsupportedOperationException();
+    }
+}
